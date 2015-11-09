cc297046a1cd6a0c65cf6a2c267777115c94a6ce
Dmitry Patsura <talk@dmtry.me>
2015-11-09 21:55:35 +0300

    Start rewriting to HttpServer class

--- server.php
+++ server.php
@@ -11,42 +11,64 @@ $dnsResolver = $dnsResolverFactory->createCached('8.8.8.8', $loop);
 $factory = new React\HttpClient\Factory();
 $client = $factory->create($loop, $dnsResolver);
 
-$userCache = new SplFixedArray(1000);
-
 $http = new React\Http\Server($socket);
-$http->on('request', function (\React\Http\Request $request, \React\Http\Response $response) use($client, &$userCache) {
-    $response->writeHead(200, array('Content-Type' => 'text/plain'));
 
-    $requestParameters = $request->getQuery();
-    $id = isset($requestParameters['id']) ? (int) $requestParameters['id'] : 1;
+class HttpServer
+{
+    /**
+     * @var SplFixedArray
+     */
+    protected $userCache;
 
-    if (isset($userCache[$id])) {
-        $response->write(json_encode($userCache[$id]));
-        $response->end("\n");
-    } else {
-        $httpRequest = $client->request('GET', 'https://api.dmtry.me/api/users/' . $id);
-        $httpRequest->on('response', function (\React\HttpClient\Response $httpResponse) use($response, &$userCache) {
-            $httpResponse->on('data', function ($data, \React\HttpClient\Response $httpResponse) use($response, &$userCache) {
-                if ($data instanceof \GuzzleHttp\Psr7\Stream && $httpResponse->getCode() == 200) {
-                    $data = $data->getContents();
+    /**
+     * @var
+     */
+    protected $client;
 
-                    $response->write('User');
-                    $response->write('Code' . $httpResponse->getCode());
-                    $response->write('Data' . $data);
-
-                    $user = json_decode($data)->result;
-                    $userCache[(int) $user->id] = $user;
-                } else {
-                    $response->write('Bad request' . $httpResponse->getCode());
-                }
-
-                $response->end("\n");
-//            $response->close();
-            });
-        });
-        $httpRequest->end();
+    public function __construct($client)
+    {
+        $this->userCache = new SplFixedArray(1000);
+        $this->client = $client;
     }
-});
+
+    public function request(\React\Http\Request $request, \React\Http\Response $response)
+    {
+        $response->writeHead(200, array('Content-Type' => 'text/plain'));
+
+        $requestParameters = $request->getQuery();
+        $id = isset($requestParameters['id']) ? (int) $requestParameters['id'] : 1;
+
+        if (isset($this->userCache[$id])) {
+            $response->write(json_encode($this->userCache[$id]));
+            $response->end("\n");
+        } else {
+            $httpRequest = $this->client->request('GET', 'https://api.dmtry.me/api/users/' . $id);
+            $httpRequest->on('response', function (\React\HttpClient\Response $httpResponse) use($response) {
+                $httpResponse->on('data', function ($data, \React\HttpClient\Response $httpResponse) use($response) {
+                    if ($data instanceof \GuzzleHttp\Psr7\Stream && $httpResponse->getCode() == 200) {
+                        $data = $data->getContents();
+
+                        $response->write('User');
+                        $response->write('Code' . $httpResponse->getCode());
+                        $response->write('Data' . $data);
+
+                        $user = json_decode($data)->result;
+                        $this->userCache[(int) $user->id] = $user;
+                    } else {
+                        $response->write('Bad request' . $httpResponse->getCode());
+                    }
+
+                    $response->end("\n");
+//            $response->close();
+                });
+            });
+            $httpRequest->end();
+        }
+    }
+}
+
+$httpServer = new HttpServer($client);
+$http->on('request', [$httpServer, 'request']);
 
 $socket->listen(8080);
 $loop->run();
